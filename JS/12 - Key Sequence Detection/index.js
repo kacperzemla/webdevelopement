b1d6682280a58628390400407b9faeb59ba71438
Kacper <kacperpeper@gmail.com>
2021-08-04 18:17:41 +0200

    some fun with JS keycode

---
+++ JS/12 - Key Sequence Detection/index.js
@@ -0,0 +1,10 @@
+const pressed = ['s','c','a','f','h','i','k']
+const code = 'wesbos'
+
+// window.addEventListener('keyup', (e) =>{
+//   console.log(e.key);
+//   pressed.push(e.key)
+//   pressed.splice(-code.length - 1 , pressed.length- code.length)
+//   console.log(pressed)
+// })
+console.log(pressed[-1])
